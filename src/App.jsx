84474966ea3a8108fd57bd327db6b18f547fddf2
hayaman3 <kevin.haya@yahoo.com>
2023-05-29 23:49:24 +0800

    added hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,80 @@
-// import { useEffect, useState } from 'react';
-
-import './styles.css';
+import { useState } from 'react';
+import './style.css';
 
 export default function App() {
+	const [newItem, setNewItem] = useState('');
+	const [todos, setTodos] = useState([]);
+
+	const handleSubmit = e => {
+		e.preventDefault();
+
+		setTodos(currentTodos => {
+			return [
+				...currentTodos,
+				{
+					id: crypto.randomUUID(),
+					title: newItem,
+					completed: false,
+				},
+			];
+		});
+
+		setNewItem('');
+	};
+
+	const toggleTodo = (id, completed) => {
+		setTodos(currentTodos => {
+			return currentTodos.map(todo => {
+				if (todo.id === id) {
+					return { ...todo, completed };
+				}
+				return todo;
+			});
+		});
+	};
+
+	const deleteTodo = id => {
+		setTodos(currentTodos => {
+			return currentTodos.filter(todo => todo.id !== id);
+		});
+	};
+
 	return (
 		<>
-			<form className="new-item-form">
+			<form onSubmit={handleSubmit} className="new-item-form">
 				<div className="form-row">
 					<label htmlFor="item">New Item</label>
-					<input type="text" id="item" />
+					<input
+						value={newItem}
+						onChange={e => setNewItem(e.target.value)}
+						type="text"
+						id="item"
+					/>
 				</div>
 				<button className="btn">Add</button>
 			</form>
 			<h1 className="header">Todo List</h1>
 			<ul className="list">
-				<li>
-					<label>
-						<input type="checkbox" />
-						Item 1
-					</label>
-					<button className="btn btn-dandger">Delete</button>
-				</li>
-				<li>
-					<label>
-						<input type="checkbox" />
-						Item 1
-					</label>
-					<button className="btn btn-dandger">Delete</button>
-				</li>
+				{todos.length === 0 && 'No Todos'}
+				{todos.map(todo => {
+					return (
+						<li key={todo.id}>
+							<label>
+								<input
+									type="checkbox"
+									checked={todo.completed}
+									onChange={e => toggleTodo(todo.id, e.target.checked)}
+								/>
+								{todo.title}
+							</label>
+							<button
+								onClick={() => deleteTodo(todo.id)}
+								className="btn btn-dandger">
+								Delete
+							</button>
+						</li>
+					);
+				})}
 			</ul>
 		</>
 	);
