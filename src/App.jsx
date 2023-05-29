ee8b02d57f8504419f315ce08d3e9e21797846ab
hayaman3 <kevin.haya@yahoo.com>
2023-05-29 22:20:24 +0800

    saving before refractor

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,30 @@ import './styles.css';
 export default function App() {
 	return (
 		<>
-			<div>asdasd</div>
+			<form className="new-item-form">
+				<div className="form-row">
+					<label htmlFor="item">New Item</label>
+					<input type="text" id="item" />
+				</div>
+				<button className="btn">Add</button>
+			</form>
+			<h1 className="header">Todo List</h1>
+			<ul className="list">
+				<li>
+					<label>
+						<input type="checkbox" />
+						Item 1
+					</label>
+					<button className="btn btn-dandger">Delete</button>
+				</li>
+				<li>
+					<label>
+						<input type="checkbox" />
+						Item 1
+					</label>
+					<button className="btn btn-dandger">Delete</button>
+				</li>
+			</ul>
 		</>
 	);
 }
